2a82389f05272f371ede0d5cde32ab27dea624fb
LoliKonch <ibugydydygig@gmail.com>
2023-09-19 09:42:11 +0300

    SignUpController new data validation

--- src/main/java/chat/local/javalocalchat/SignUpController.java
+++ src/main/java/chat/local/javalocalchat/SignUpController.java
@@ -1,5 +1,6 @@
 package chat.local.javalocalchat;
 
+import customexceptions.InvalidDataException;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
@@ -7,7 +8,6 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 
 import java.io.IOException;
-import java.util.regex.Pattern;
 
 /**
  * Sign up window controller class
@@ -17,22 +17,6 @@ import java.util.regex.Pattern;
 public class SignUpController {
 
     private final ChangeWindow  ChangeWindow= new ChangeWindow();
-
-    /**
-     * Field regular expression for E-mail
-     */
-    private static final Pattern RCF2822_MAIL_PATTERN = Pattern.compile(
-            "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"" +
-                    "(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")" +
-                    "@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])" +
-                    "?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]" +
-                    "?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\" +
-                    "\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
-
-    /**
-     * Field regular expression for login
-     */
-    private static final Pattern LOGIN_PATTERN = Pattern.compile("[a-zA-Z0-9_]");
 
     @FXML
     private Button backButton;
@@ -71,54 +55,43 @@ public class SignUpController {
 
         // The event listener for the creating an account request button can open an E-mail_confirmation window
         signUpButton.setOnAction(event ->{
-            if (loginField.getText() != null && !loginField.getText().trim().isEmpty() &&
-                passwordField.getText() != null && !passwordField.getText().trim().isEmpty() &&
-                mailField.getText() != null && !mailField.getText().trim().isEmpty()) {
 
-                if (LOGIN_PATTERN.matcher(loginField.getText()).find()) {
-
-                    if (RCF2822_MAIL_PATTERN.matcher(mailField.getText()).matches()) {
-
-                        Client.setUsername(loginField.getText().trim());
-                        Client.setPassword(passwordField.getText().trim());
-                        Client.setEmail(mailField.getText().trim());
-
-                        try {
-                            // Start connection to server
-                            Client.startClient();
-                            // Send creating an account request to server
-                            Client.sendMessage(Client.getUsername());
-                            Client.sendMessage("sign_up" +
-                                    "|" + Client.getUsername() +
-                                    "|" + Client.getPassword() +
-                                    "|" + Client.getEmail());
-
-                            String answer = Client.waitMessage();
-                            if (answer.equals("successful_pre_sign_up")) {
-                                ChangeWindow.changeWindowTo(sideBackground,
-                                        "E-mail_confirmation.fxml",
-                                        true);
-
-                            } else {
-                                ExceptionBox.createExceptionBox(sideBackground,
-                                        "         Username already occupied");
-                                Client.closeEverything();
-                            }
-                        } catch (IOException e) {
-                            Client.closeEverything();
-                            ExceptionBox.createExceptionBox(sideBackground,
-                                    "        Unable to connect to server" +
-                                            "\n         Please try again later");
-                        }
-                    } else {
-                        ExceptionBox.createExceptionBox(sideBackground, "Invalid E-mail address");
-                    }
-                } else {
-                    ExceptionBox.createExceptionBox(sideBackground, "                    Invalid Login");
-                }
-            } else {
-                ExceptionBox.createExceptionBox(sideBackground, "          All fields must be filled in");
+            // Validating login password and E-mail
+            try {
+                Client.setEmail(Validators.loginValidator(loginField));
+                Client.setPassword(Validators.passwordValidator(passwordField));
+                Client.setPassword(Validators.emailValidator(mailField));
+            } catch (InvalidDataException e) {
+                ExceptionBox.createExceptionBox(sideBackground, e.getMessage());
+                return;
             }
+
+            try {
+                // Start connection to server
+                Client.startClient();
+                // Send creating an account request to server
+                Client.sendMessage(Client.getUsername());
+                Client.sendMessage("sign_up" +
+                        "|" + Client.getUsername() +
+                        "|" + Client.getPassword() +
+                        "|" + Client.getEmail());
+
+                String answer = Client.waitMessage();
+                if (answer.equals("successful_pre_sign_up")) {
+                    ChangeWindow.changeWindowTo(sideBackground, "E-mail_confirmation.fxml", true);
+
+                } else {
+                    ExceptionBox.createExceptionBox(sideBackground,
+                            "         Username already occupied");
+                                        Client.closeEverything();
+                }
+            } catch (IOException e) {
+                Client.closeEverything();
+                ExceptionBox.createExceptionBox(sideBackground,
+                        "        Unable to connect to server" +
+                                "\n         Please try again later");
+            }
+
         });
     }
 }
