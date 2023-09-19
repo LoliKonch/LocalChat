c1151f1156ebb88f0fcd9346e09bed421326bdbb
LoliKonch <ibugydydygig@gmail.com>
2023-09-19 10:41:39 +0300

    SignInController new data validation

--- src/main/java/chat/local/javalocalchat/SignInController.java
+++ src/main/java/chat/local/javalocalchat/SignInController.java
@@ -1,5 +1,6 @@
 package chat.local.javalocalchat;
 
+import customexceptions.InvalidDataException;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
@@ -8,7 +9,6 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 
 import java.io.IOException;
-import java.util.regex.Pattern;
 
 /**
  * Sign in window controller class
@@ -18,21 +18,6 @@ import java.util.regex.Pattern;
 public class SignInController {
 
     private final ChangeWindow  ChangeWindow= new ChangeWindow();
-
-    /**
-     * Field regular expression for login
-     */
-    private static final Pattern LOGIN_PATTERN = Pattern.compile("[a-zA-Z0-9_]");
-
-    /**
-     * Field regular expression for IP-address
-     */
-    private static final Pattern IP_PATTERN = Pattern.compile(
-            "^(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\." +
-                  "(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\." +
-                  "(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\." +
-                  "(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
-    );
 
     @FXML
     private Hyperlink forgotPassword;
@@ -77,52 +62,41 @@ public class SignInController {
         // The event listener for the enter button can open a Chat window
         signInButton.setOnAction(event ->{
 
-            if (loginField.getText() != null && !loginField.getText().trim().isEmpty() &&
-                passwordField.getText() != null && !passwordField.getText().trim().isEmpty() &&
-                IPAddressField.getText() != null && !IPAddressField.getText().trim().isEmpty()) {
+            // Validating login password and IP
+            try {
+                Client.setEmail(Validators.loginValidator(loginField));
+                Client.setPassword(Validators.passwordValidator(passwordField));
+                Client.setIPAddress(Validators.ipValidator(IPAddressField));
+            } catch (InvalidDataException e) {
+                ExceptionBox.createExceptionBox(sideBackground, e.getMessage());
+                return;
+            }
 
-                if (LOGIN_PATTERN.matcher(loginField.getText()).find()) {
+            try {
+                // Start connection to server
+                Client.startClient();
+                // Send enter to chat request to server
+                Client.sendMessage(Client.getUsername());
+                Client.sendMessage(
+                        "sign_in" +
+                        "|" + Client.getUsername() +
+                        "|" + Client.getPassword()
+                );
 
-                    if (IP_PATTERN.matcher(IPAddressField.getText()).find()) {
+                String answer = Client.waitMessage();
+                if (answer.equals("successful_sign_in")) {
+                    ChangeWindow.changeWindowTo(sideBackground, "Chat.fxml");
 
-                        Client.setUsername(loginField.getText().trim());
-                        Client.setPassword(passwordField.getText().trim());
-                        Client.setIPAddress(IPAddressField.getText().trim());
-
-                        try {
-                            // Start connection to server
-                            Client.startClient();
-                            // Send enter to chat request to server
-                            Client.sendMessage(Client.getUsername());
-                            Client.sendMessage(
-                                    "sign_in" +
-                                    "|" + Client.getUsername() +
-                                    "|" + Client.getPassword()
-                            );
-
-                            String answer = Client.waitMessage();
-                            if (answer.equals("successful_sign_in")) {
-                                ChangeWindow.changeWindowTo(sideBackground, "Chat.fxml");
-
-                            } else {
-                                ExceptionBox.createExceptionBox(sideBackground,
-                                        "         Incorrect login or password");
-                                Client.closeEverything();
-                            }
-                        } catch (IOException e) {
-                            Client.closeEverything();
-                            ExceptionBox.createExceptionBox(sideBackground,
-                                    "        Unable to connect to server" +
-                                            "\n             Please try again later");
-                        }
-                    } else {
-                        ExceptionBox.createExceptionBox(sideBackground, "                Invalid IP Address");
-                    }
                 } else {
-                    ExceptionBox.createExceptionBox(sideBackground, "                    Invalid Login");
+                    ExceptionBox.createExceptionBox(sideBackground,
+                            "         Incorrect login or password");
+                            Client.closeEverything();
                 }
-            } else {
-                ExceptionBox.createExceptionBox(sideBackground, "          All fields must be filled in");
+            } catch (IOException e) {
+                Client.closeEverything();
+                ExceptionBox.createExceptionBox(sideBackground,
+                        "        Unable to connect to server" +
+                        "\n             Please try again later");
             }
         });
     }
