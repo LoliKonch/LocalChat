b04e5613b93e5b810bff7ae91669e474c0e0df8e
LoliKonch <ibugydydygig@gmail.com>
2023-09-19 09:41:20 +0300

    Validators new validators

--- src/main/java/chat/local/javalocalchat/Validators.java
+++ src/main/java/chat/local/javalocalchat/Validators.java
@@ -2,6 +2,8 @@ package chat.local.javalocalchat;
 
 import customexceptions.InvalidDataException;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 import java.util.regex.Pattern;
 
 
@@ -19,6 +21,10 @@ public class Validators {
                     "?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\" +
                     "\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
 
+    private static final Pattern LOGIN_PATTERN = Pattern.compile("[a-zA-Z0-9_]");
+
+    private static final Pattern PASSWORD_PATTERN = Pattern.compile("[a-zA-Z0-9_!@#$%]");
+
     public static String confirmationCodeValidator(PasswordField confirmationCodeField) throws InvalidDataException {
         if (confirmationCodeField.getText() != null && !confirmationCodeField.getText().trim().isEmpty()){
             return confirmationCodeField.getText().trim();
@@ -27,12 +33,36 @@ public class Validators {
         }
     }
 
-    public static String EmailValidator(PasswordField mailField) throws InvalidDataException {
+    public static String emailValidator(TextField mailField) throws InvalidDataException {
         if (mailField.getText() != null && !mailField.getText().trim().isEmpty()){
             if (RCF2822_MAIL_PATTERN.matcher(mailField.getText()).matches()) {
                 return mailField.getText().trim();
             } else {
                 throw new InvalidDataException("Invalid E-mail address");
+            }
+        } else {
+            throw new InvalidDataException("All fields must be filled in");
+        }
+    }
+
+    public static String loginValidator(TextField loginField) throws InvalidDataException {
+        if (loginField.getText() != null && !loginField.getText().trim().isEmpty()){
+            if (LOGIN_PATTERN.matcher(loginField.getText()).matches()) {
+                return loginField.getText().trim();
+            } else {
+                throw new InvalidDataException("Invalid Login");
+            }
+        } else {
+            throw new InvalidDataException("All fields must be filled in");
+        }
+    }
+
+    public static String passwordValidator(PasswordField passwordField) throws InvalidDataException {
+        if (passwordField.getText() != null && !passwordField.getText().trim().isEmpty()){
+            if (PASSWORD_PATTERN.matcher(passwordField.getText()).matches()) {
+                return passwordField.getText().trim();
+            } else {
+                throw new InvalidDataException("Invalid Password");
             }
         } else {
             throw new InvalidDataException("All fields must be filled in");
